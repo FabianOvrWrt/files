8b0fcf2c09cf940dce74617d993c4b4554a78800
Igzok <jul.reich43@openmailbox.org>
2014-11-04 23:17:38 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
-$("#listdata").searcher({
-    itemSelector: ".details",
-    textSelector:  "h1", // the text is within the item element (li) itself
-    inputSelector: "#listsearchinput"
+$("#data").searcher({
+    itemSelector: "h1","span",
+    textSelector:  "",
+    inputSelector: "#searchinput"
 });
